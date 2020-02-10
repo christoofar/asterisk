989bcff5dbb929114f21b18f39c3a5960a1e3bbb
Christopher Sawyer <christoofar@gmail.com>
2020-02-09 21:47:15 -0500

    IAX2 extension now works!

--- start.sh
+++ start.sh
@@ -3,18 +3,19 @@ SIPPASSWORD=$(uuidgen)
 IPADDRESS=$(hostname --ip-address)
 [ "$(ls -A /etc/asterisk)" ] && echo "etc folder has files, not doing anything" || {
 	tar -xvzf /root/configs.tar.gz -C /etc/asterisk;
-	sed -i "s/changme/$SIPPASSWORD/g" /etc/asterisk/sip.conf
-	sed -i "s/IPADDRESS/$IPADDRESS/g" /etc/asterisk/sip.conf
+	sed -i "s/changme/$SIPPASSWORD/g" /etc/asterisk/sip.conf;
+	sed -i "s/IPADDRESS/$IPADDRESS/g" /etc/asterisk/sip.conf;
+	sed -i "s/TEST123/$SIPPASSWORD/g" /etc/asterisk/iax.conf;
 	echo;
-	echo Asterisk setup with a default configuration;
-	echo To help you with connectivity tests we have set up the following;
-	echo inbound config: ;
+	echo Asterisk is setup with a default configuration;
+	echo To help you with connectivity tests we have created an;
+	echo IAX extension for you to test with: ;
 	echo -------------------------;
-	echo Protocol: SIP;
+	echo Protocol: IAX;
 	echo 
-	echo Extension: testuser;
+	echo Extension: TEST123;
 	echo Password:  $SIPPASSWORD;
-	echo Port: 5060 tcp or udp;
+	echo Port: 4569 udp;
 	echo -------------------------;
 	echo Asterisk is running on $(hostname --ip-address);
 	echo Hostname: $(hostname);
